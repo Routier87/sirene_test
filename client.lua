a31d6160be4ca8356d9666fa3a6d61dca50838fb
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2025-12-16 23:54:03 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,9 +1,12 @@
 RegisterCommand("sirene_test", function()
+    local dureeSirene = 91000 -- 1 min 31 s (ajuste si besoin)
+
     for i = 1, 3 do
         SendNUIMessage({ action = "play" })
-        Wait(100000) -- 1 min 40 s
+        Wait(dureeSirene)
+
         if i < 3 then
-            Wait(5000) -- pause 5 s
+            Wait(5000) -- pause 5 secondes entre chaque cycle
         end
     end
-end)
+end, false)

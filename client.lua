23061372dd6ec15fc0f78014eb2a89ae073203ef
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2025-12-16 23:59:54 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,9 +1,16 @@
-RegisterNetEvent("sirene_test:play")
-AddEventHandler("sirene_test:play", function()
-    local dureeSirene = 91000 -- durée réelle du .ogg
+local sireneActive = false
+
+RegisterNetEvent("sirene:play")
+AddEventHandler("sirene:play", function()
+    if sireneActive then return end
+    sireneActive = true
 
     CreateThread(function()
+        local dureeSirene = 91000 -- durée du .ogg
+
         for i = 1, 3 do
+            if not sireneActive then break end
+
             SendNUIMessage({ action = "play" })
             Wait(dureeSirene)
 
@@ -11,9 +18,29 @@ AddEventHandler("sirene_test:play", function()
                 Wait(5000)
             end
         end
+
+        sireneActive = false
     end)
 end)
 
-RegisterCommand("sirene_test", function()
-    TriggerServerEvent("sirene_test:playAll")
+RegisterNetEvent("sirene:stop")
+AddEventHandler("sirene:stop", function()
+    sireneActive = false
+    SendNUIMessage({ action = "stop" })
+end)
+
+-- === COMMANDES ===
+
+RegisterCommand("sirenon", function()
+    TriggerServerEvent("sirene:on")
 end, false)
+
+RegisterCommand("sirenoff", function()
+    TriggerServerEvent("sirene:off")
+end, false)
+
+-- === SUGGESTIONS CHAT (boutons visibles) ===
+CreateThread(function()
+    TriggerEvent('chat:addSuggestion', '/sirenon', '🔊 SIREN ON – Lancer la sirène nationale')
+    TriggerEvent('chat:addSuggestion', '/sirenoff', '⛔ SIREN OFF – Couper la sirène immédiatement')
+end)
